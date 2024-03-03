92b2292ecd334ea2b2e73ad9c8d6f8e2b3a52fe3
Renovacio3429 <sav3429@gmail.com>
2024-03-04 02:40:27 +0700

    fix

--- hw1/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw1/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class TestServiceImpl implements TestService {
     private final IOService ioService;
     private final QuestionDao questionDao;
-
+    
     @Override
     public void executeTest() {
         ioService.printLine("");
