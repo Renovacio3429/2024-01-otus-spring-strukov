3137e6b22208c396442dfa0fcb8e826e116a61ef
Renovacio3429 <sav3429@gmail.com>
2024-03-09 18:37:28 +0700

    annotation config

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,11 +1,17 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+import java.util.List;
+
 @RequiredArgsConstructor
+@Service
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
@@ -19,10 +25,30 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        for (var question : questions) {
+            var validAnswer = question.answers()
+                    .stream()
+                    .filter(Answer::isCorrect)
+                    .findFirst();
+
+            // skip question without correct answer
+            validAnswer.ifPresent(answer -> {
+                printQuestion(question);
+
+                var userAnswer = ioService.readStringWithPrompt("Write your answer: ");
+                var isAnswerValid = answer.text().equals(userAnswer);
+
+                testResult.applyAnswer(question, isAnswerValid);
+            });
         }
         return testResult;
+    }
+
+    private void printQuestion(Question q) {
+        ioService.printLine("Question: " + q.text() + "%n");
+        ioService.printLine("Possible answers: ");
+        ioService.printLine("");
+        q.answers().forEach(a -> ioService.printLine(a.text()));
+        ioService.printLine("");
     }
 }
