12c40727a024d41428afad2aeacb689e37afdbe3
Alexandr <59480386+Renovacio3429@users.noreply.github.com>
2024-03-12 16:13:04 +0700

    Hw2 (#2)
    * hw2 init template
    
    * add new module
    
    * add test dependency
    
    * annotation config
    
    * add unit test
    
    * fix imports
    
    * rm unused import
    
    * fix pr comments
    
    * fix test
    
    * fix for loop

---
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,56 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        return this.readFileByName(fileNameProvider.getTestFileName());
+    }
+
+    private List<Question> readFileByName(String fileName) {
+        var is = this.getFileFromResource(fileName);
+
+        try (var reader = new InputStreamReader(is)) {
+            var csvContext = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+
+            return csvContext
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("Something went wrong: ", e);
+        }
+    }
+
+    private InputStream getFileFromResource(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+    }
+}
